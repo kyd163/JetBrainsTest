f0bf03345982931ddd95a69876598a0e4bbca112
kyd163 <kyd163@yandex.ru>
2026-04-06 19:25:52 +0300

    MyasoedovAV AcpPage PR/ LOG

--- src/test/java/com/example/jetbrainstest/pages/AcpPage.java
+++ src/test/java/com/example/jetbrainstest/pages/AcpPage.java
@@ -1,36 +1,39 @@
 package com.example.jetbrainstest.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class AcpPage {
+    private final Logger LOG = LoggerFactory.getLogger(AcpPage.class);
     WebDriver driver;
     @FindBy(xpath = "(//span[normalize-space()='Read docs'])[1]")
     public WebElement readButton;
     @FindBy(xpath = "//*[@id=\"ch2-dialog\"]/div[3]/button[1]")
             private WebElement acceptButton;
-    String acpUrl = "https://agentclientprotocol.com/get-started/introduction";
+//    String acpUrl = "https://agentclientprotocol.com/get-started/introduction";
 
 
     public AcpPage(WebDriver driver) {
+        LOG.info("Инициализация драйвера");
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
     public boolean readButtonCheckEnabled() {
+        LOG.info("Кнопка активна");
         return readButton.isEnabled();
     }
 
     public void readButtonClick() {
+        LOG.info("Клик по кнопке Read docs");
         acceptButton.click();
         waitDownloadPage();
         readButton.click();
@@ -42,6 +45,7 @@ public class AcpPage {
 //    }
 
     public void waitDownloadPage() {
+        LOG.info("Ожидание элемента на странице");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         wait.until(ExpectedConditions.visibilityOf(readButton));
     }
