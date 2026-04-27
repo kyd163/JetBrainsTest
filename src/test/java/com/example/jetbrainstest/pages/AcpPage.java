10d812e5624ad77ac88cde9fae01263a6247dc21
kyd163 <kyd163@yandex.ru>
2026-04-27 19:41:43 +0300

    MyasoedovAV AcpPage PR/ 20 тестов по странице AcpPage

--- src/test/java/com/example/jetbrainstest/pages/AcpPage.java
+++ src/test/java/com/example/jetbrainstest/pages/AcpPage.java
@@ -5,21 +5,68 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.LoggerFactory;
 
-import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.example.jetbrainstest.MyWait.myWait;
 
 public class AcpPage {
     private final AllureLogger LOG = new AllureLogger(LoggerFactory.getLogger(AcpPage.class));
     WebDriver driver;
+    String acpUrl = "https://www.jetbrains.com/acp/";
 
-    @FindBy(xpath = "(//span[normalize-space()='Read docs'])[1]")
-    public WebElement readButton;
+    @FindBy(xpath = "(//span[text()='Read docs'])[1]")
+    public WebElement readFirstButton;
 
-    @FindBy(xpath = "(//span[text()='Read docs'])[3]")
-    private WebElement acceptButton;
+    @FindBy(xpath = "(//*[@id=\"ch2-dialog\"]//button[1]")
+    private WebElement acceptButtonCookie;
+
+    @FindBy (xpath = "(//span[text()='Read docs'])[2]")
+    private WebElement readTwoButton;
+
+    @FindBy (xpath = "//h4[text()='Junie by JetBrains']")
+    private WebElement junieButton;
+
+    @FindBy(xpath = "(//a[text()='Explore the full list'])[1]")
+    private WebElement fullListFirstButton;
+
+    @FindBy(xpath = "(//a[text()='Explore the full list'])[1]")
+    private WebElement fullListTwoButton;
+
+    @FindBy(xpath = "//h3[contains(text(),'ACP + Deep Agents')]")
+    private WebElement usingAcpButton;
+
+    @FindBy(xpath = "[class=\"btn\"]")
+    private WebElement exploreButton;
+
+    @FindBy(xpath = "//h3[contains(text(),'Cursor Joined the ACP Registry')]")
+    private WebElement cursorJoinedButton;
+
+    @FindBy(xpath = "//h3[contains(text(),'Koog x ACP')]")
+    private WebElement koogButton;
+
+    @FindBy(xpath = "//button[@data-test='next-button']")
+    private WebElement nextButton;
+
+    @FindBy(xpath = "//button[@aria-label='Play video']")
+    private WebElement playButton;
+
+    @FindBy (xpath = "//span[text()='United States']")
+    private WebElement unitedStatesButton;
+
+    @FindBy (xpath = "//svg[@data-test='down-icon']")
+    private WebElement dropDown;
+
+    @FindBy (xpath = "//span[text()='Australia']")
+    private WebElement elementOfDropDown;
+
+    @FindBy (css = "button.footer-choose-button")
+    private WebElement chooseButton;
+
+    @FindBy (xpath = "//span[text()='Australia']")
+    private WebElement australiaButton;
+
+
 
     public AcpPage(WebDriver driver) {
         LOG.info("Инициализация драйвера");
@@ -27,21 +74,114 @@ public class AcpPage {
         PageFactory.initElements(driver, this);
     }
 
-    public boolean readButtonCheckEnabled() {
-        LOG.infoWithScreenshot("Кнопка активна");
-        return readButton.isEnabled();
+    public boolean readFirstButtonCheckEnabled() {
+        LOG.infoWithScreenshot("Первая кнопка ReadDocs активна");
+        return readFirstButton.isEnabled();
     }
 
-    public void readButtonClick() {
-        LOG.infoWithScreenshot("Клик по кнопке Read docs");
-        acceptButton.click();
-        waitDownloadPage();
-        readButton.click();
+    public void readFirstButtonClick() {
+        LOG.infoWithScreenshot("Нажатие на первую кнопку ReadDocs");
+//        acceptButtonCookie.click();
+        myWait(10).visible(readFirstButton);
+        readFirstButton.click();
     }
 
-    public void waitDownloadPage() {
-        LOG.infoWithScreenshot("Ожидание элемента на странице");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOf(readButton));
+    public void checkUrl(){
+        LOG.infoWithScreenshot("Проверка url страницы");
+         String url = driver.getCurrentUrl();
+         assertEquals(acpUrl, url, "Открылась неверная вкладка");
+    }
+
+    public boolean readTwoButtonCheckEnabled() {
+        LOG.infoWithScreenshot("Вторая кнопка ReadDocs активна");
+        return readTwoButton.isEnabled();
+    }
+
+    public void  readTwoButtonClick() {
+        LOG.infoWithScreenshot("Нажатие на вторую кнопку ReadDocs");
+//        acceptButtonCookie.click();
+        myWait(10).visible(readTwoButton);
+        readTwoButton.click();
+    }
+
+    public boolean junieButtonCheck(){
+        LOG.infoWithScreenshot("Кнопка Junie by JetBrains активна");
+        return junieButton.isEnabled();
+    }
+
+    public void junieButtonClick(){
+        LOG.infoWithScreenshot("Нажатие на кнопку Junie by JetBrains");
+        junieButton.click();
+    }
+
+    public boolean fullListFirstButtonCheck(){
+        LOG.infoWithScreenshot("Первая кнопка Explore the full list активна");
+        return fullListFirstButton.isEnabled();
+    }
+
+    public void fullListFirstButtonClick(){
+        LOG.infoWithScreenshot("Нажатие на первую кнопку Explore the full list");
+        fullListFirstButton.click();
+    }
+
+    public boolean fullListTwoButtonCheck(){
+        LOG.infoWithScreenshot("Первая кнопка Explore the full list активна");
+        return fullListTwoButton.isEnabled();
+    }
+
+    public void fullListTwoButtonClick(){
+        LOG.infoWithScreenshot("Нажатие на вторую кнопку Explore the full list");
+        fullListTwoButton.click();
+    }
+
+    public boolean usingAcpBlog(){
+        LOG.infoWithScreenshot("Открытие блога UsingAcp и проверка видимости кнопки");
+        usingAcpButton.click();
+        acceptButtonCookie.click();
+        myWait(10).visible(exploreButton);
+        return exploreButton.isEnabled();
+    }
+
+    public void exploreButtonClick(){
+        LOG.infoWithScreenshot("Нажатие на кнопку ExploreMore");
+        exploreButton.click();
+    }
+
+    public boolean cursorJoinedBlog (){
+        LOG.infoWithScreenshot("Открытие блога CursorJoined и проверка видимости кнопки");
+        cursorJoinedButton.click();
+        acceptButtonCookie.click();
+        myWait(10).visible(exploreButton);
+        return exploreButton.isDisplayed();
+    }
+
+    public boolean koogBlog(){
+        LOG.infoWithScreenshot("Открытие блога Koog и проверка видимости кнопки");
+        koogButton.click();
+        acceptButtonCookie.click();
+        myWait(10).visible(exploreButton);
+        return exploreButton.isEnabled();
+    }
+
+    public boolean buttonNext(){
+        LOG.infoWithScreenshot("Проверка видимости стрелки и нажатие");
+        nextButton.click();
+        return nextButton.isEnabled();
+    }
+
+    public boolean playVideo(){
+        LOG.infoWithScreenshot("Запуск видео");
+        playButton.click();
+        return playButton.isEnabled();
+    }
+
+    public boolean countryButton(){
+        LOG.infoWithScreenshot("Иконка Country поменяла своё значение");
+        unitedStatesButton.click();
+        dropDown.click();
+        elementOfDropDown.click();
+        chooseButton.click();
+        return australiaButton.isEnabled();
     }
 }
+
--- src/test/java/com/example/jetbrainstest/tests/AcpPageTest.java
+++ src/test/java/com/example/jetbrainstest/tests/AcpPageTest.java
@@ -3,6 +3,7 @@ package com.example.jetbrainstest.tests;
 import com.example.jetbrainstest.MyExtension;
 import com.example.jetbrainstest.pages.AcpPage;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -10,26 +11,124 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MyExtension.class)
 public class AcpPageTest extends BaseTest {
-    private AcpPage AcpPage;
+    private AcpPage acpPage;
 
     @BeforeEach
     @Override
     public void setUp() {
         super.setUp();
         getDriver().get("https://www.jetbrains.com/acp/");
-        AcpPage = new AcpPage(getDriver());
+        acpPage = new AcpPage(getDriver());
     }
 
     @Test
+    @DisplayName("Проверка адреса страницы")
+    public void checkAcpUrl(){
+        acpPage.checkUrl();
+    }
+
+    @Test
+    @DisplayName("Проверка наличия первой кнопки ReadDocs")
     public void readButtonCheck() {
-        AcpPage.waitDownloadPage();
-        assertTrue(AcpPage.readButtonCheckEnabled(), "Кнопка Read docs отсутствует");
+        acpPage.readFirstButtonCheckEnabled();
+        assertTrue(acpPage.readFirstButtonCheckEnabled(), "Первая кнопка ReadВocs отсутствует");
     }
 
     @Test
+    @DisplayName("Нажатие на первую кнопку ReadDocs")
     public void readButtonClickTest() {
-        AcpPage.readButtonClick();
-        AcpPage.waitDownloadPage();
+        acpPage.readFirstButtonClick();
+    }
 
+    @Test
+    @DisplayName("Проверка наличия второй кнопки ReadDocs")
+    public void readTwoButtonCheck(){
+        acpPage.readTwoButtonCheckEnabled();
+        assertTrue(acpPage.readTwoButtonCheckEnabled(),"Вторая кнопка ReadDocs отсутствует");
+    }
+
+    @Test
+    @DisplayName("Нажатие на вторую кнопку ReadDocs")
+    public void readTwoButtonClickTest(){
+        acpPage.readTwoButtonClick();
+    }
+
+    @Test
+    @DisplayName("Проверка наличие кнопки Junie by JetBrains")
+    public void junieButtonCheckTest(){
+        acpPage.junieButtonCheck();
+    }
+
+    @Test
+    @DisplayName("Нажатие на кнопку Junie by JetBrains")
+    public void junieButtonClickTest(){
+        acpPage.junieButtonClick();
+    }
+
+    @Test
+    @DisplayName("Проверка наличия первой кнопки Explore the full list")
+    public void fullListFirstButtonCheckTest(){
+        acpPage.fullListFirstButtonCheck();
+    }
+
+    @Test
+    @DisplayName("Нажатие на первую кнопку Explore the full list")
+    public void fullListFirstButtonClickTest(){
+        acpPage.fullListFirstButtonClick();
+    }
+
+    @Test
+    @DisplayName("Проверка наличия второй кнопки Explore the full list")
+    public void fullListTwoButtonCheckTest(){
+        acpPage.fullListTwoButtonCheck();
+    }
+
+    @Test
+    @DisplayName("Нажатие на вторую кнопку Explore the full list")
+    public void fullListTwoButtonClickTest(){
+        acpPage.fullListTwoButtonClick();
+    }
+
+    @Test
+    @DisplayName("Открытие блога UsingAcp и проверка видимости кнопки")
+    public void usingAppBlogTest(){
+        acpPage.usingAcpBlog();
+    }
+
+    @Test
+    @DisplayName("Нажатие на кнопку ExploreMore")
+    public void exploreButtonClickTest(){
+        acpPage.exploreButtonClick();
+    }
+
+    @Test
+    @DisplayName("Открытие блога CursorJoined и проверка видимости кнопки")
+    public void cursorJoinedBlogTest(){
+        acpPage.cursorJoinedBlog();
+    }
+
+    @Test
+    @DisplayName("Открытие блога Koog и проверка видимости кнопки")
+    public void koogBlogTest(){
+        acpPage.koogBlog();
+    }
+
+    @Test
+    @DisplayName("Иконка стрелочки")
+    public void nextButtonTest(){
+        acpPage.buttonNext();
+    }
+
+    @Test
+    @DisplayName("Запуск видео")
+    public void playVideo(){
+        acpPage.playVideo();
+    }
+
+    @Test
+    @DisplayName("Изменение названия кнопки Country, после смены страны")
+    public void countryButtonTest(){
+        acpPage.countryButton();
     }
 }
+
